f8f7686c93011c83b9a669d975af9d606f44e90c
Balram <kurmibalram936@gmail.com>
2021-11-01 03:10:52 +0530

    sharetop done

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import "./Feed.css"
+import "./Feed.css";
+import Share from '../share/Share';
+import Post from "../posts/Post"
 export default function Feed() {
     return (
         <div className="feed">
-            Feed
+            <div className="feedWrapper">
+                <Share/>
+                <Post/>
+            </div>
         </div>
     )
 }
---
+++ src/components/share/Share.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Share.css";
+import {Theaters,Label,Room,EmojiEmotions} from "@mui/icons-material";
+
+export default function Share() {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img className="shareProfileImg" src="/assets/persons/3.jpg" alt="" />
+          <input placeholder="what's in your mind" className="shareInput" />
+        </div>
+        <hr className="shareHr"/>
+        <div className="shareBottom">
+            <div className="shareOptions">
+                <div className="shareOption">
+                    <Theaters htmlColor="tomato" className="shareIcon"/>
+                    <span className="shareOptionText">Photo/Video</span>
+                </div>
+                <div className="shareOption">
+                    <Label htmlColor="blue" className="shareIcon"/>
+                    <span className="shareOptionText">Tag</span>
+                </div>
+                <div className="shareOption">
+                    <Room htmlColor="green" className="shareIcon"/>
+                    <span className="shareOptionText">Location</span>
+                </div>
+                <div className="shareOption">
+                    <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
+                    <span className="shareOptionText">Feelings</span>
+                </div>
+            </div>
+            <button className="shareButton">Share</button>
+        </div>
+      </div>
+    </div>
+  );
+}
